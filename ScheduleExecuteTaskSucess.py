885164007ad3f46ec5bf8bc11c1bca79ee9e3272
antmoves <34129231+antmoves@users.noreply.github.com>
2023-04-13 14:01:06 +0800

    Update ScheduleExecuteTaskSucess.py

--- ScheduleExecuteTaskSucess.py
+++ ScheduleExecuteTaskSucess.py
@@ -8,10 +8,10 @@ if __name__ == '__main__':
     # send list
     sm.mailto_list = [os.getenv("MAILTO_LIST")]
     sm.mail_title = 'Hey subject'
-    sm.mail_content = '尊重的客户：' \
-                   '         执行时间：'+time.asctime() \
-                   '         任务状态：任务执行成功' \
-                   '                                   感谢使用'
+    sm.mail_content = '尊重的客户：\r' \
+                   '         执行时间：'+time.asctime()+'         \r' \
+                   '         任务状态：任务执行成功\r' \
+                   '                                   感谢使用\r'
     mm = sm.Mailer(sm.mailto_list, sm.mail_title, sm.mail_content)
     res = mm.sendMail()
 
